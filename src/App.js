d9fb0613058d345699f8798d390399bc14d7e797
GhostEsso <lilipitaham@gmail.com>
2023-08-10 10:24:03 +0000

    RandomQuote component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
+import RandomQuote from './components/RandomQuote';
 
 function App() {
-  return <Calculator />;
+  return (
+    <div>
+      <Calculator />
+      <RandomQuote />
+    </div>
+  );
 }
 
 export default App;
