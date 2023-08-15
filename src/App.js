fba75dccad9537323474cb34c2bd5eef54749310
GhostEsso <lilipitaham@gmail.com>
2023-08-15 10:11:40 +0000

    Home.js modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import RandomQuote from './components/RandomQuote';
+import Home from './components/Home';
 
-const App = () => {
-
-}
+const App = () => (
+  <>
+    <nav>
+      <h1>Math Magicians</h1>
+      <ul>
+        <li><Link exact to="/Home">Home</Link></li>
+        <li><Link exact to="/Calculator">Calculator</Link></li>
+        <li><Link exact to="/RandomQuote">Quote</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route exact path="/Home" element={<Home />} />
+      <Route exact path="/Calculator" element={<Calculator />} />
+      <Route exact path="/RandomQuote" element={<RandomQuote />} />
+    </Routes>
+  </>
+);
 
 export default App;
--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function RandomQuote() {
   const [quote, setQuote] = useState(null);
@@ -9,22 +9,22 @@ function RandomQuote() {
     const dataFetch = async () => {
       try {
         const response = await fetch(
-          "https://api.api-ninjas.com/v1/quotes?category=happiness",
+          'https://api.api-ninjas.com/v1/quotes?category=happiness',
           {
-            method: "GET",
+            method: 'GET',
             headers: {
-              "X-Api-Key": "V8FzqJZj+pOipGZbfuVsdw==vKvb8VNKkoGE0fLQ",
-              "Content-type": "application/json",
+              'X-Api-Key': 'V8FzqJZj+pOipGZbfuVsdw==vKvb8VNKkoGE0fLQ',
+              'Content-type': 'application/json',
             },
-          }
+          },
         );
         const quotes = await response.json();
         if (quotes.length > 0) {
           setQuote(quotes[0].quote);
           setAuthor(quotes[0].author); // Set the author of the quote
         } else {
-          setQuote("");
-          setAuthor("");
+          setQuote('');
+          setAuthor('');
         }
       } catch (err) {
         setError(err.message);
