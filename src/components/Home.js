8dbc4874b716e26a93d3c636b5cbdb645a70f6db
GhostEsso <lilipitaham@gmail.com>
2023-08-15 09:41:00 +0000

    Home section added

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import './Home.css';
+
+const Home = () => (
+    <section className='Homepage'>
+        <h1> Welcome to our page!</h1>
+        <p className='firstparagraph'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore sapiente aperiam libero tempora autem aspernatur 
+            quibusdam mollitia tempore iure facilis,
+            fugit, rem amet. Delectus nam sit 
+            aspernatur illum ab et?
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusamus quod ut, eius consequuntur ex pariatur quasi
+            totam aspernatur placeat quos itaque ipsa ratione provident!
+            Illo totam laborum ipsa a veniam.
+        </p>
+
+        <p className='secondparagraph'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae nam officiis, id officia totam tenetur illo
+            dolores magnam. Sapiente tempore cum voluptate praesentium
+            veritatis deserunt placeat ex iure nemo ipsum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Animi est quia magnam, ratione, inventore facilis delectus impedit cum,
+            rem debitis officia fugiat ex? Eligendi provident dolore exercitationem,
+            nihil nostrum! Voluptates!
+        </p>
+    </section>
+);
+
+export default Home;
